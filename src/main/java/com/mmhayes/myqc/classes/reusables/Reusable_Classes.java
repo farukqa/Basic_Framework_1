e10da98a94ca2b916d481126d683130f45433c06
faruklmu17 <farukqmul@gmail.com>
2022-01-27 21:06:37 -0500

    Added a comment which explains what the reusable class does

--- src/main/java/com/mmhayes/myqc/classes/reusables/Reusable_Classes.java
+++ src/main/java/com/mmhayes/myqc/classes/reusables/Reusable_Classes.java
@@ -1,5 +1,6 @@
 package com.mmhayes.myqc.classes.reusables;
 
 public class Reusable_Classes {
+    // all the reusable classes (like click, send keys, right click, taking screenshot etc will be listed in this class
 }
 
